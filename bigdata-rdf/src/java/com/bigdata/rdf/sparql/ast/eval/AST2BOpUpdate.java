a48c8b30bca2bcb4a164d6c896106329e028faf5
beebs-systap <beebs@systap.com>
2015-09-15 13:21:13 -0400

    Update for missed file during merge.

--- bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/AST2BOpUpdate.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/sparql/ast/eval/AST2BOpUpdate.java
@@ -130,12 +130,10 @@ import com.bigdata.rdf.store.AbstractTripleStore;
 import com.bigdata.rdf.store.BD;
 import com.bigdata.rdf.store.BigdataOpenRDFBindingSetsResolverator;
 import com.bigdata.striterator.Chunkerator;
-import com.bigdata.rdf.sparql.ast.eval.MutableTupleQueryResult;
 
 import cutthecrap.utils.striterators.ICloseableIterator;
 import cutthecrap.utils.striterators.Resolver;
 import cutthecrap.utils.striterators.Striterator;
-import info.aduna.iteration.CloseableIteration;
 
 /**
  * Class handles SPARQL update query plan generation.
@@ -241,12 +239,12 @@ public class AST2BOpUpdate extends AST2BOpUtility {
          */
         PipelineOp left = null;
 		int updateIndex = 0;
+		// @see BLZG-1446
+		final DeleteInsertWhereStats deleteInsertWhereStats = new DeleteInsertWhereStats();
 		for (Update op : updateRoot) {
 
 //          log.error("\nbefore op=" + op + "\n" + context.conn.getTripleStore().dumpStore());
 
-			long connectionFlushNanos = 0L;
-			long batchResolveNanos = 0L;
 			if (updateIndex > 0) {
 
 				/*
@@ -262,10 +260,7 @@ public class AST2BOpUpdate extends AST2BOpUtility {
 				 * 
 				 * @see https://sourceforge.net/apps/trac/bigdata/ticket/558
 				 */
-				final long t1 = System.nanoTime();
 				context.conn.flush();
-				final long t2 = System.nanoTime();
-				connectionFlushNanos = t2 - t1;
 
 				/*
 				 * We need to re-resolve any RDF Values appearing in this UPDATE
@@ -277,12 +272,11 @@ public class AST2BOpUpdate extends AST2BOpUtility {
 				op = (Update) new ASTBatchResolveTermsOptimizer().optimize(context,
 				      new QueryNodeWithBindingSet(op, context.getBindings()))
 						.getQueryNode();
-				batchResolveNanos = System.nanoTime() - t2;
 
 			}
 			
 			final long begin = System.nanoTime();
-			final DeleteInsertWhereStats deleteInsertWhereStats = new DeleteInsertWhereStats(); // @see BLZG-1446.
+			
 			Throwable cause = null;
             try {
                 // convert/run the update operation.
@@ -293,7 +287,7 @@ public class AST2BOpUpdate extends AST2BOpUtility {
                 // notify listener(s)
                 final long elapsed = System.nanoTime() - begin;
                 context.conn.getSailConnection().fireEvent(
-                        new SPARQLUpdateEvent(op, elapsed, connectionFlushNanos, batchResolveNanos, cause, deleteInsertWhereStats));
+                        new SPARQLUpdateEvent(op, elapsed, cause, deleteInsertWhereStats));
                 if (t instanceof Exception)
                     throw (Exception) t;
                 if (t instanceof RuntimeException)
@@ -305,7 +299,7 @@ public class AST2BOpUpdate extends AST2BOpUtility {
 			
 			// notify listener(s)
             context.conn.getSailConnection().fireEvent(
-                    new SPARQLUpdateEvent(op, elapsed, connectionFlushNanos, batchResolveNanos, cause, deleteInsertWhereStats));
+                    new SPARQLUpdateEvent(op, elapsed, cause, deleteInsertWhereStats));
 
 			updateIndex++;
 			
@@ -628,20 +622,17 @@ public class AST2BOpUpdate extends AST2BOpUtility {
 				 * associated with the SailConnection in case the view is
 				 * isolated by a transaction.
 				 */
-
-				// Note: Blocks until the result set is materialized.
-				final long beginWhereClauseNanos = System.nanoTime();
-				final MutableTupleQueryResult result = new MutableTupleQueryResult(
-						ASTEvalHelper.evaluateTupleQuery(
-								context.conn.getTripleStore(), astContainer,
-								context.getQueryBindingSet()/* bindingSets */));
-				deleteInsertWhereStats.whereNanos.set(System.nanoTime() - beginWhereClauseNanos);
 				
 				// If the query contains a nativeDistinctSPO query hint then
 				// the line below unfortunately isolates the query so that the hint does
 				// not impact any other execution, this is hacked by putting a property on the query root.
 				
-				final boolean nativeDistinct = astContainer.getOptimizedAST().getProperty(ConstructNode.Annotations.NATIVE_DISTINCT,
+				final MutableTupleQueryResult result = new MutableTupleQueryResult(
+						ASTEvalHelper.evaluateTupleQuery(
+								context.conn.getTripleStore(), astContainer,
+								context.getQueryBindingSet()/* bindingSets */));
+				
+				boolean nativeDistinct = astContainer.getOptimizedAST().getProperty(ConstructNode.Annotations.NATIVE_DISTINCT,
 						ConstructNode.Annotations.DEFAULT_NATIVE_DISTINCT);
 
                 try {
