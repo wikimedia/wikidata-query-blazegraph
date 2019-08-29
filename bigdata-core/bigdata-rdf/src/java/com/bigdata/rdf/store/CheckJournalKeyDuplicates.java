5fa8718543c0a14d70f91981a109356d57bcf957
Igor Kim <igorkim78@gmail.com>
2019-08-29 05:04:00 +0500

    Refined CheckJournalKeyDuplicates utility
    Change-Id: Ib4264d04823a08958a347013997b87b223b8c196

--- bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/store/CheckJournalKeyDuplicates.java
+++ bigdata-core/bigdata-rdf/src/java/com/bigdata/rdf/store/CheckJournalKeyDuplicates.java
@@ -25,6 +25,7 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -40,17 +41,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.annotation.Nullable;
-
 import org.apache.log4j.Logger;
 
 import com.bigdata.Banner;
 import com.bigdata.btree.AbstractBTree;
-import com.bigdata.btree.ICheckpointProtocol;
 import com.bigdata.btree.ITuple;
 import com.bigdata.btree.ITupleIterator;
 import com.bigdata.journal.ICommitRecord;
 import com.bigdata.journal.Journal;
 import com.bigdata.journal.Options;
+import com.bigdata.rdf.lexicon.Id2TermTupleSerializer;
+import com.bigdata.rdf.lexicon.Term2IdTupleSerializer;
+import com.bigdata.rdf.model.BigdataValueFactoryImpl;
 import com.google.common.base.Optional;
 import com.google.common.base.Stopwatch;
 
@@ -73,6 +75,14 @@ public class CheckJournalKeyDuplicates {
      *            <code>[-namespace <i>namespace</i>] propertyFile</code>
      *            where
      *            <dl>
+     *            <dt>-checkStatements</dt>
+     *            <dd>Checks statement indices for duplication as well. If not specified,
+     *            the check will run only against lexicon indices.</dd>
+     *            </dd>
+     *            <dt>-checkUsingSet</dt>
+     *            <dd>Checks statement indices for duplication as well. If not specified,
+     *            the check will run only against lexicon indices.</dd>
+     *            </dd>
      *            <dt>-namespace</dt>
      *            <dd>The namespace of the KB instance. If not specified,
      *            the check will run against all the namespaces in the journal.</dd>
@@ -89,7 +99,7 @@ public class CheckJournalKeyDuplicates {
 
             Optional<Journal> optionalJnl = arguments.openJournal();
             if (optionalJnl.isPresent()) {
-                checkJournal(optionalJnl.get(), arguments.namespace, arguments.checkUsingSet);
+                checkJournal(optionalJnl.get(), arguments.namespace, arguments.checkUsingSet, arguments.checkStatements);
             } else {
                 log.debug("Journal " + arguments.getJournalPath() + " does not exist");
             }
@@ -100,14 +110,14 @@ public class CheckJournalKeyDuplicates {
     }
 
 
-    private static void checkJournal(Journal journal, String namespace, boolean checkUsingSet) throws Exception {
+    private static void checkJournal(Journal journal, String namespace, boolean checkUsingSet, boolean checkStatements) throws Exception {
         Stopwatch stopwatch = Stopwatch.createStarted();
         AtomicLong duplicates = new AtomicLong();
         Journal jnl = journal;
         try {
             log.info("Processing the journal: " + jnl.getFile());
             for (String nm : getNamespacesToCheck(jnl, namespace)) {
-                checkNamespace(jnl, nm, duplicates, checkUsingSet);
+                checkNamespace(jnl, nm, duplicates, checkUsingSet, checkStatements);
             }
         } finally {
             jnl.close();
@@ -122,7 +132,7 @@ public class CheckJournalKeyDuplicates {
         return singletonList(namespace);
     }
 
-    private static void checkNamespace(final Journal jnl, final String namespace, final AtomicLong duplicates, final boolean checkUsingSet) throws Exception {
+    private static void checkNamespace(final Journal jnl, final String namespace, final AtomicLong duplicates, final boolean checkUsingSet, final boolean checkStatements) throws Exception {
         log.debug("Copying " + namespace);
         ExecutorService pool = Executors.newFixedThreadPool(5);
         log.info("Running verify (TripleStoreUtility.modelsEqual)");
@@ -133,13 +143,10 @@ public class CheckJournalKeyDuplicates {
 
         while (nitr.hasNext()) {
             final String name = nitr.next();
-            if (name.startsWith(namespace)) {
+            if (name.startsWith(namespace) && (checkStatements || !name.contains(".spo."))) {
                 log.debug("Processing "+name);
                 final AbstractBTree ndx = jnl.getIndex(name);//WithCommitRecord , commitRecord);
                 if (ndx != null) {
-                    ndx.insert(new byte[] {1,2,3,4,5,6,7,8,9}, new byte[] {1,2,3});
-                    ndx.insert(new byte[] {1,2,3,4,5,6,7,8,9}, new byte[] {4,5,6});
-                    ndx.insert(new byte[] {1,2,3,4,5,6,7,8,10}, new byte[] {1,2,3});
                     tasks.add(new Callable<Void>() {
                         @Override
                         public Void call() {
@@ -151,31 +158,74 @@ public class CheckJournalKeyDuplicates {
                             long startTimeChunk = startTime;
                             Set<ByteBuffer> prevKeys = new HashSet<>();
                             Set<ByteBuffer> prevValues = new HashSet<>();
+                            Term2IdTupleSerializer serTerm2Id = null;
+                            Id2TermTupleSerializer serId2Term = null;
+                            if (checkUsingSet) {
+                                if (name.endsWith("TERM2ID")) {
+                                    serTerm2Id = new Term2IdTupleSerializer();
+                                } else if (name.endsWith("ID2TERM")) {
+                                    serId2Term = new Id2TermTupleSerializer(name, new BigdataValueFactoryImpl());
+                                }
+                            }
                             byte[] prevKey = new byte[0];
                             byte[] prevValue = null;
                             while (bitr.hasNext()) {
                                 ITuple<?> t = bitr.next();
                                 byte[] key = t.getKey();
+                                Object deserializedKey = null;
+                                Object deserializedValue = null;
+                                try {
+                                    if (name.endsWith("TERM2ID")) {
+                                        ByteBuffer tempArr = ByteBuffer.wrap(new byte[key.length+3]);
+                                        byte[] keyFixed = tempArr.array();
+                                        System.arraycopy(key, 1, keyFixed,0, key.length-1);
+                                        deserializedKey = new String(keyFixed, StandardCharsets.US_ASCII); //bigdataValueSerializer.deserialize(keyFixed);
+                                        deserializedValue = serTerm2Id.deserialize(t);
+                                    } else if (name.endsWith("ID2TERM")) {
+                                        deserializedKey = serId2Term.deserializeKey(t);
+                                        deserializedValue = serId2Term.deserialize(t);
+                                    }
+                                    log.warn("Idx " + name + " Check " 
+                                        + " tuple " + deserializedKey + " to " + deserializedValue
+                                        + " key " + Arrays.toString(key)
+                                        + " value " + Arrays.toString(t.getValue()));
+                                } catch(Exception e) {
+                                    log.error("Error",e);
+                                }
                                 if (checkUsingSet && (name.endsWith("TERM2ID") || name.endsWith("ID2TERM"))) {
                                     // wrap byte[] as ByteBuffer to provide it with proper hashcode and compare
                                     ByteBuffer keyAsBuffer = ByteBuffer.wrap(key);
                                     if (prevKeys.contains(keyAsBuffer)) {
-                                        log.warn("Idx " + name + " Duplicated key in set " + Arrays.toString(key) +" value " + Arrays.toString(t.getValue()));
+                                        log.warn("Idx " + name + " Duplicated key in set " 
+                                            + " tuple " + deserializedKey + " to " + deserializedValue
+                                            + " key " + Arrays.toString(key)
+                                            + " value " + Arrays.toString(t.getValue()));
+                                        duplicates.incrementAndGet();
                                     }
                                     prevKeys.add(keyAsBuffer);
                                     // wrap byte[] as ByteBuffer to provide it with proper hashcode and compare
                                     ByteBuffer valueAsBuffer = ByteBuffer.wrap(t.getValue());
                                     if (prevValues.contains(valueAsBuffer)) {
-                                        log.warn("Idx " + name + " Duplicated value in set " + Arrays.toString(key) +" value " + Arrays.toString(t.getValue()));
+                                        log.warn("Idx " + name + " Duplicated value in set " 
+                                                + " tuple " + deserializedKey + " to " + deserializedValue
+                                                + " key " + Arrays.toString(key)
+                                                + " value " + Arrays.toString(t.getValue()));
+                                        duplicates.incrementAndGet();
                                     }
                                     prevValues.add(valueAsBuffer);
                                 }
                                 if (Arrays.equals(prevKey, key)) {
                                     if (prevValue != null) {
-                                        log.warn("Idx " + name + " Duplicated key " + Arrays.toString(prevKey) +" value " + Arrays.toString(prevValue));
+                                        // Log the first duplicated record
+                                        log.warn("Idx " + name + " Duplicated "
+                                                + " key " + Arrays.toString(prevKey) 
+                                                + " value " + Arrays.toString(prevValue));
                                         prevValue = null;
                                     }
-                                    log.warn("Idx " + name + " Duplicated key " + Arrays.toString(key) +" value " + Arrays.toString(t.getValue()));
+                                    // Log the subsequent duplicated records
+                                    log.warn("Idx " + name + " Duplicated " 
+                                            + " key " + Arrays.toString(key) 
+                                            + " value " + Arrays.toString(t.getValue()));
                                     duplicates.incrementAndGet();
                                 } else {
                                     prevKey = key;
@@ -225,16 +275,19 @@ public class CheckJournalKeyDuplicates {
         @Nullable public final String namespace;
         private final String propertyFileName;
         private final boolean checkUsingSet;
+        private final boolean checkStatements;
 
-        private Arguments(@Nullable String namespace, String propertyFileName, boolean checkUsingSet) {
+        private Arguments(@Nullable String namespace, String propertyFileName, boolean checkUsingSet, boolean checkStatements) {
             this.namespace = namespace;
             this.propertyFileName = propertyFileName;
             this.checkUsingSet = checkUsingSet;
+            this.checkStatements = checkStatements;
         }
 
         public static Arguments parseArguments(String[] args) {
             String namespace = null;
             boolean checkUsingSet = false;
+            boolean checkStatements = false;
             int i = 0;
 
             while (i < args.length) {
@@ -246,6 +299,9 @@ public class CheckJournalKeyDuplicates {
                             break;
                         case "-checkUsingSet":
                             checkUsingSet = true;
+                            break;
+                        case "-checkStatements":
+                            checkStatements = true;
                             break;
                         default:
                             log.debug("Unknown argument: " + arg);
@@ -266,7 +322,7 @@ public class CheckJournalKeyDuplicates {
 
             String propertyFileName = args[i];
 
-            return new Arguments(namespace, propertyFileName, checkUsingSet);
+            return new Arguments(namespace, propertyFileName, checkUsingSet, checkStatements);
         }
 
         private static void usage() {
